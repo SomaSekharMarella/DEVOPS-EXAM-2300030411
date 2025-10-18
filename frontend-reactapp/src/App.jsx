fdb962dc102ba0eda574597ca18186de40a809ef
SomaSekharMarella <2300030411@kluniversity.in>
2025-10-18 10:56:58 +0530

    add

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import NavBar from "./components/NavBar.jsx";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
